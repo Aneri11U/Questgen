f8bf5b5cad5890cb13404e0647f9ea2bce8020d3
Dev Makwana <devmakwana7704@gmail.com>
2024-06-08 18:35:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from pprint import pprint
+from Questgen import main
+
+st.set_page_config(
+    page_title='Questgen',
+    page_icon= ':fire:',
+)
+
+st.title(body='Question Generator')
+
+input_text = st.text_area(
+    label='Enter text from which questions are to be generated',
+    value = 'Sachin Tendulkar is the best batsman in the history of cricket. Sachin is from Mumbai. Sachin has two children.'
+)
+
+qg = main.QGen()
+
+payload = {
+    'input_text' : input_text
+}
+
+output = qg.predict_mcq(payload=payload)
+
+st.subheader(body='*Generated Questions are:*', divider='orange')
+for question in output['questions']:
+    st.write(f"{question['question_statement']}")
+
+if st.toggle(label='Show Total Output'):
+    st.write(output)
